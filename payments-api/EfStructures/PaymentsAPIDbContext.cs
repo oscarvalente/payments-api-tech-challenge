4999a3a9012ab5ccf8c8118c13fe7a884298065d
Óscar Valente <oscar@fidel.uk>
2022-12-05 10:06:00 +0000

    scaffold models by connection string

--- payments-api/EfStructures/PaymentsAPIDbContext.cs
+++ payments-api/EfStructures/PaymentsAPIDbContext.cs
@@ -24,8 +24,7 @@ namespace PaymentsAPI.EfStructures
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;user=oscar;password=gitCheckout2022!;database=payments_api", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"));
+                optionsBuilder.UseMySql("name=ConnectionStrings:Default", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"));
             }
         }
 
--- payments-api/tools/build.sh
+++ payments-api/tools/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mysql -h "localhost" -u "oscar" -p < "./tools/create-database.sql"
-dotnet ef dbcontext scaffold "Server=localhost;User=oscar;Password=gitCheckout2022!;Database=payments_api" Pomelo.EntityFrameworkCore.MySql -d -c PaymentsAPIDbContext --context-dir EfStructures -o Entities -f
+dotnet ef dbcontext scaffold Name=ConnectionStrings:Default Pomelo.EntityFrameworkCore.MySql -d -c PaymentsAPIDbContext --context-dir EfStructures -o Entities -f
 dotnet clean payments-api.csproj
 dotnet build payments-api.csproj
 
